4d73a8bb579a385b5ff6d285628cd260a2f99636
asimurzin <asimurzin@gmail.com>
2010-08-12 09:44:00 +0000

    to change FOAM_VERSION() and FOAM_BRANCH functions to FOAM_VERSION( cmp, version), FOAM_REF_VERSION( cmp, version ), FOAM_BRANCH_VERSION( name, cm, version )

--- Foam/applications/solvers/basic/r1_6/scalarTransportFoam.py
+++ Foam/applications/solvers/basic/r1_6/scalarTransportFoam.py
@@ -108,12 +108,12 @@ def main_standalone( argc, argv ):
 #--------------------------------------------------------------------------------------
 import sys, os
 from Foam import FOAM_VERSION
-if FOAM_VERSION() == "010600" :
+if FOAM_VERSION( "==", "010600" ):
    if __name__ == "__main__" :
       argv = sys.argv
       if len( argv ) > 1 and argv[ 1 ] == "-test":
          argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'r1.6', 'basic', 'scalarTransportFoam', 'pitzDaily' )
+         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'propogated', 'r1.6', 'basic', 'scalarTransportFoam', 'pitzDaily' )
          argv = [ __file__, "-case", test_dir ]
          pass
       os._exit( main_standalone( len( argv ), argv ) )
